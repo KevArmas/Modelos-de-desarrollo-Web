f96bb89205a7308415847686ccad5cb31bbf7e21
KevArmas <Kevin.armas@alumno.buap.mx>
2023-11-23 14:02:57 -0600

    Carga con Mongo DB

--- ModelosDeDesarrolloWeb/Actividad 9/db/connection.py
+++ ModelosDeDesarrolloWeb/Actividad 9/db/connection.py
@@ -4,3 +4,5 @@ from pymongo import MongoClient
 
 #Instancia de tipo MongoClient (si no se le asignan argumentos se conecta al localhost por default)
 db_client= MongoClient()
+
+#db_client= MongoClient("mongodb+srv://kevarmashernandez:61JJhPeB6A5khz4m@modelosweb.3jke5d8.mongodb.net/?retryWrites=true&w=majority").kevarmashernandez
